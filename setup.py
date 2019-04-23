9a14a7e63e31a2a42dfeaecd0a9bbbc8d2189265
Drew Perttula <drewp@bigasterisk.com>
2019-04-22 22:53:24 -0700

    release 0.3.0

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
  
 setup(
     name='twisted_sse',
-    version='0.2.0',
+    version='0.3.0',
     packages=['twisted_sse'],
     package_dir={'twisted_sse': ''},
     requires=['twisted'],
-    url='https://projects.bigasterisk.com/twisted_sse/twisted_sse-0.2.0.tar.gz',
+    url='https://projects.bigasterisk.com/twisted_sse/twisted_sse-0.3.0.tar.gz',
     author='Drew Perttula',
     author_email='drewp@bigasterisk.com',
 )
